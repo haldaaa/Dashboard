eabca9610ebcf1b8faa1b2f66c1f61a95af6c60b
haldaaa <gasmi.fares@gmail.com>
2023-01-04 21:52:23 +0100

    Clear code

--- resources/views/commande/commande-liste.blade.php
+++ resources/views/commande/commande-liste.blade.php
@@ -2,24 +2,26 @@
 
 
 @section ('contenu')
-<div class="container">
-  <h1 style="center"> Listes des commandes : </h1> </br> </br>
-<table class="table ">
-    <thead>
-      <tr>
-        <th scope="col">Commercial </th>
-        <th scope="col">Nombre de commandes</th>
-        <th scope="col">Bénéfice total</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-@foreach ($liste as $data)
-        <td>{{ $data->nom }}</td>
-        <td> {{ $data->nbre_commande}} </td>
-        <td> {{ $data ->total_vente }}</td>
-      </tr>
-@endforeach
+
+  <div class="container">
+    <h1 style="center"> Listes des commandes : </h1> </br> </br>
+      
+      <table class="table ">
+       <thead>
+          <tr>
+            <th scope="col">Commercial </th>
+            <th scope="col">Nombre de commandes</th>
+            <th scope="col">Bénéfice total</th>
+          </tr>
+        </thead>
+      <tbody>
+          <tr>
+              @foreach ($liste as $data)
+                <td>{{ $data->nom }}</td>
+                <td> {{ $data->nbre_commande}} </td>
+                <td> {{ $data ->total_vente }}</td>
+         </tr>
+              @endforeach
     </tbody>
 
   </table>
@@ -27,24 +29,30 @@
  
     <div class="row">
   
-      <div class="col-md-6 cadre cadreDetailCommande">
-        <p> Derniière commande : : </p>
+      <div class="col-md-5 cadre cadreDetailCommande">
+        <p> Dernière commande : : </p>
       @if(isset($vendeurCommande))
         <p> Effectué par : {{ $vendeurCommande }}  (ID :{{ $idLastCommande }} )</p>
         @endif
       
       </div>
-      <div class="col-md-6 cadre">
+      <div class="col-md-3 cadre">
         <p> Commande à inspecter : </p>
         @if(isset($commandeSelect))
         <select id="select" name="select_commandeId" class="custom-select " aria-describedby="selectHelpBlock">
+
           @foreach ($commandeSelect  as $nom )
               <option value="{{ $nom -> id }} ">{{ $nom -> id }} </option>
           @endforeach
         @endif
+        
       </div>
 
-    </div>
+      
+
+
+    </div> <!-- Div Row -->
+    
   </div>
 
 @endsection
